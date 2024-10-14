442605b877ac205ab15135c240186498ac1c6bb5
Axel179-cr <axelgarcia4511@gmail.com>
2024-10-13 20:13:44 -0600

    Update ip-addresses.php

--- ip-addresses.php
+++ ip-addresses.php
@@ -6,7 +6,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Obtenemos los datos enviados desde la página web
     $datos = json_decode(file_get_contents('php://input'), true);
 
-    // Guardamos los IP addresses en un archivo de texto
-    file_put_contents('ip-addresses.txt', $datos['ipAddresses'] . PHP_EOL, FILE_APPEND);
+    // Guardamos los IP addresses en el archivo txt
+    $contenido = file_get_contents('ip-addresses.txt');
+    $ipAddresses = explode(PHP_EOL, $contenido);
+
+    $ipAddresses = array_merge($ipAddresses, $datos['ipAddresses']);
+    $contenido = implode(PHP_EOL, $ipAddresses);
+
+    file_put_contents('ip-addresses.txt', $contenido);
 }
 ?>
